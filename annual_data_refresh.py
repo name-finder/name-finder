daa302cff38114021d93d3182a127c6ff1860ad2
Devon Ankar <devon.ankar@gmail.com>
2022-03-13 20:50:18 -0400

    Add read table on page to see if year has been added

--- annual_data_refresh.py
+++ annual_data_refresh.py
@@ -1,24 +1,34 @@
 import zipfile
+from datetime import datetime
 from time import sleep
 
+import pandas as pd
 import requests
 
 
-def main():
-    urls = (
-        'https://www.ssa.gov/oact/babynames/names.zip',
-        'https://www.ssa.gov/oact/babynames/territory/namesbyterritory.zip',
-    )
+def _run(session):
+    # read table on page to see if year has been added
+    response = session.get('https://www.ssa.gov/oact/babynames/limits.html')
+    df = pd.read_html(response.text)[0]
+    if df.iloc[0, 0] != datetime.today().year - 1:
+        return
 
-    session = requests.Session()
-
-    for url in urls:
+    sleep(3)
+    # if year has been added, then download files
+    for url in (
+            'https://www.ssa.gov/oact/babynames/names.zip',
+            'https://www.ssa.gov/oact/babynames/territory/namesbyterritory.zip',
+    ):
         filepath = 'data/' + url.rsplit('/', 1)[1]
         open(filepath, 'wb').write(session.get(url).content)
         sleep(3)
         with zipfile.ZipFile(filepath) as z:
             z.extractall(filepath[:-4])
 
+
+def main():
+    session = requests.Session()
+    _run(session)
     session.close()
 
 
