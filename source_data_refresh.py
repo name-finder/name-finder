c4bc1ae1ad7a98c6f1f0d592d6b0578d4343a11d
Devon Ankar <devon.ankar@gmail.com>
2022-04-01 11:18:36 -0400

    Add function _refresh_actuarial_tables()

--- source_data_refresh.py
+++ source_data_refresh.py
@@ -7,12 +7,12 @@ import requests
 from finder import _MAX_YEAR
 
 
-def _run(session):
+def _refresh_babynames(session):
     # compare to website
     response = session.get('https://www.ssa.gov/oact/babynames/limits.html')
     table = pd.read_html(response.text)[0]
     if _MAX_YEAR >= int(table.iloc[0, 0]):
-        return
+        return False
 
     sleep(3)
     # if year has been added, then download files
@@ -26,10 +26,21 @@ def _run(session):
         with zipfile.ZipFile(filepath) as z:
             z.extractall(filepath[:-4])
 
+    return True
+
+
+def _refresh_actuarial_tables(session):
+    response = session.get('https://www.ssa.gov/oact/HistEst/Death/2021/DeathProbsE_M_Hist_TR2021.txt')
+    lines = [line.split() for line in response.text.splitlines()[1:]]
+    columns = lines[0]
+    table = pd.DataFrame(lines[1:], columns=columns)
+    table.to_csv('data/actuarial.csv', index=False)
+
 
 def main():
     session = requests.Session()
-    _run(session)
+    if _refresh_babynames(session):
+        _refresh_actuarial_tables(session)
     session.close()
 
 
