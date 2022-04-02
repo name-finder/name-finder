1a4794d2386ff4e9ed81c8fa0ad40cdb2f21b790
Devon Ankar <devon.ankar@gmail.com>
2022-04-02 11:35:45 -0400

    Remove method search_historic()

--- finder.py
+++ finder.py
@@ -270,12 +270,8 @@ class Displayer(Loader):
             return
 
         summary = df.sort_values('number', ascending=False)
+        # summary = self._calcd[self._calcd.name.isin(summary.name)].sort_values('year')
         return summary.to_dict('records') if _OUTPUT_RECORDS else summary
-
-    def search_historic(self, *args):
-        summary = self.search(*args)
-        historic = self._calcd[self._calcd.name.isin(summary.name)].sort_values('year')
-        return historic.to_dict('records') if _OUTPUT_RECORDS else historic
 
     def predict_age(
             self,
