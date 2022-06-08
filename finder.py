38aaebfef743dd077ac810fd4c4156283d33fd5c
Devon Ankar <devon.ankar@gmail.com>
2022-06-08 18:19:05 -0400

    Add condition

--- finder.py
+++ finder.py
@@ -171,16 +171,18 @@ class Displayer(Loader):
             historic = df[['year', 'number', 'number_f', 'number_m', 'ratio_f', 'ratio_m']].copy()
             for s in ('f', 'm'):
                 historic[f'ratio_{s}'] = historic[f'ratio_{s}'].apply(lambda x: round(x, 2))
-            blocks = '▓', '▒', '░'
-            historic['ratio_bars'] = (
-                    historic.ratio_f.apply(lambda x: 'f ' + int(round(x * 50)) * blocks[0]) +
-                    historic.ratio_m.apply(lambda x: int(round(x * 50)) * blocks[1] + ' m') +
-                    historic.year.apply(str).apply(lambda x: f' {x}')
-            )
             output['historic'] = list(historic.to_dict('records')) if OUTPUT_RECORDS else historic
-            historic4bars = historic[historic.year.apply(lambda x: (x >= MAX_YEAR - 80) & (x % 5 == 0))] if not (
-                    after or before or year) else historic
-            output['display'] = '  \n'.join((output['display'], '. Ratio Bars', *historic4bars.ratio_bars))
+
+            if not (output['ratios']['f'] == 1 or output['ratios']['m'] == 1):
+                blocks = '▓', '▒', '░'
+                historic['ratio_bars'] = (
+                        historic.ratio_f.apply(lambda x: 'f ' + int(round(x * 50)) * blocks[0]) +
+                        historic.ratio_m.apply(lambda x: int(round(x * 50)) * blocks[1] + ' m') +
+                        historic.year.apply(str).apply(lambda x: f' {x}')
+                )
+                historic4bars = historic[historic.year.apply(lambda x: (x >= MAX_YEAR - 80) & (x % 5 == 0))] if not (
+                        after or before or year) else historic
+                output['display'] = '  \n'.join((output['display'], '. Ratio Bars', *historic4bars.ratio_bars))
         return output
 
     def compare(self, names: tuple, *args, **kwargs) -> dict:
