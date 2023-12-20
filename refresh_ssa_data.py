43d97310065d777b9f8271c2d8064d6f416ee4ec
Devon Ankar <devon.ankar@gmail.com>
2023-12-19 20:56:57 -0500

    refresh_ssa_data: use class

--- refresh_ssa_data.py
+++ refresh_ssa_data.py
@@ -4,52 +4,57 @@ from time import sleep
 import pandas as pd
 import requests
 
-from core import MAX_YEAR
+from core import MAX_YEAR, Filepaths
 
 
-def _refresh_babynames(session: requests.Session) -> bool:
-    # compare to website
-    response = session.get('https://www.ssa.gov/oact/babynames/limits.html')
-    table = pd.read_html(response.text)[0]
-    if MAX_YEAR >= int(table.iloc[0, 0]):
-        return False
+class SsaDataRefresher:
+    def refresh(self) -> None:
+        self._open_session()
+        if self._refresh_name_data():
+            self._refresh_actuarial_data()
+        self._close_session()
 
-    sleep(3)
-    # if year has been added, then download files
-    for url in (
-            'https://www.ssa.gov/oact/babynames/names.zip',
-            'https://www.ssa.gov/oact/babynames/territory/namesbyterritory.zip',
-    ):
-        filepath = 'data/' + url.rsplit('/', 1)[1]
-        open(filepath, 'wb').write(session.get(url).content)
+    def _open_session(self) -> None:
+        self._session = requests.Session()
+
+    def _close_session(self) -> None:
+        self._session.close()
+
+    def _refresh_name_data(self) -> bool:
+        # compare to website
+        table = pd.read_html(self._session.get('https://www.ssa.gov/oact/babynames/limits.html').text)[0]
+        if MAX_YEAR >= int(table.iloc[0, 0]):
+            return False
+
         sleep(3)
-        with zipfile.ZipFile(filepath) as z:
-            z.extractall(filepath[:-4])
-    return True
+        # if year has been added, then download files
+        for url in (
+                'https://www.ssa.gov/oact/babynames/names.zip',
+                'https://www.ssa.gov/oact/babynames/territory/namesbyterritory.zip',
+        ):
+            filepath = 'data/' + url.rsplit('/', 1)[1]
+            with open(filepath, 'wb') as f:
+                f.write(self._session.get(url).content)
+            sleep(3)
+            with zipfile.ZipFile(filepath) as z:
+                z.extractall(filepath[:-4])
+        return True
 
-
-def _refresh_actuarial(session: requests.Session) -> None:
-    url = 'https://www.ssa.gov/oact/HistEst/CohLifeTables/{0}/CohLifeTables_{1}_Alt2_TR{0}.txt'
-    columns = {'Year': 'year', 'x': 'age', 'l(x)': 'survivors'}
-    for s in ('F', 'M'):
-        response = session.get(url.format(MAX_YEAR + 1, s))
-        if not response.ok:
-            return
-        sleep(3)
-        lines = [line.split() for line in response.text.splitlines()]
-        df = pd.DataFrame(lines[6:], columns=lines[5])
-        df = df[list(columns.keys())].rename(columns=columns)
-        for col in df.columns:
-            df[col] = df[col].map(int)
-        df.to_csv(f'data/actuarial/{s.lower()}.csv', index=False)
-
-
-def main() -> None:
-    session = requests.Session()
-    if _refresh_babynames(session):
-        _refresh_actuarial(session)
-    session.close()
+    def _refresh_actuarial_data(self) -> None:
+        url = 'https://www.ssa.gov/oact/HistEst/CohLifeTables/{0}/CohLifeTables_{1}_Alt2_TR{0}.txt'
+        columns = {'Year': 'year', 'x': 'age', 'l(x)': 'survivors'}
+        for s in ('F', 'M'):
+            response = self._session.get(url.format(MAX_YEAR + 1, s))
+            if not response.ok:
+                return
+            sleep(3)
+            lines = [line.split() for line in response.text.splitlines()]
+            df = pd.DataFrame(lines[6:], columns=lines[5])
+            df = df[list(columns.keys())].rename(columns=columns)
+            for col in df.columns:
+                df[col] = df[col].map(int)
+            df.to_csv(Filepaths.ACTUARIAL.format(sex=s.lower()), index=False)
 
 
 if __name__ == '__main__':
-    main()
+    SsaDataRefresher().refresh()
