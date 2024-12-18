4ee180c2780aafba8c90701dc012a37924a50a9a
Devon Ankar <devon.ankar@gmail.com>
2024-12-18 15:58:59 -0500

    Streamline SSA data download/refresh

--- ssa_data_downloader.py
+++ ssa_data_downloader.py
@@ -2,39 +2,35 @@ import zipfile
 from time import sleep
 
 import pandas as pd
-import requests
+from requests import Session
 
-from core import Year, Filepath
+from core import Filepath
 
 
-class SsaDataRefresher:
-    def __init__(self) -> None:
-        self.refreshed = False
+class SsaDataDownloader:
+    def __init__(self, max_year: int) -> None:
+        self._max_year: int = max_year
 
-    def refresh(self) -> None:
+    def download(self) -> None:
         self._open_session()
-        self.refreshed = self._refresh_name_data()
-        if self.refreshed:
-            self._refresh_applicants_data()
-            self._refresh_actuarial_data()
+        self._download_name_data()
+        self._download_applicants_data()
+        self._download_actuarial_data()
         self._close_session()
+        return
 
     def _open_session(self) -> None:
-        self._session = requests.Session()
+        self._session: Session = Session()
+        return
 
     def _close_session(self) -> None:
         self._session.close()
+        return
 
-    def _refresh_name_data(self) -> bool:
-        # compare to website
-        table = pd.read_html('https://www.ssa.gov/oact/babynames/limits.html')[0]
-        if Year.MAX_YEAR >= int(table.iloc[0, 0]):
-            return False
-
-        sleep(3)
-        # if year has been added, then download files
+    def _download_name_data(self) -> None:
         for url in (
                 'https://www.ssa.gov/oact/babynames/names.zip',
+                'https://www.ssa.gov/oact/babynames/state/namesbystate.zip',
                 'https://www.ssa.gov/oact/babynames/territory/namesbyterritory.zip',
         ):
             filepath = Filepath.DATA_DIR + url.rsplit('/', 1)[1]
@@ -43,26 +39,21 @@ class SsaDataRefresher:
             sleep(3)
             with zipfile.ZipFile(filepath) as z:
                 z.extractall(filepath[:-4])
-        return True
+        return
 
     @staticmethod
-    def _refresh_applicants_data() -> None:
-        # compare to website
+    def _download_applicants_data() -> None:
         table = pd.read_html('https://www.ssa.gov/oact/babynames/numberUSbirths.html')[0]
-        if Year.MAX_YEAR >= table.iloc[-1, 0]:
-            return
-
-        sleep(3)
-        # if year has been added, save
         table = table.rename(columns=dict((col, ''.join(col.split())) for col in table.columns)).rename(columns=dict(
             Yearofbirth='year', Male='number_m', Female='number_f', Total='number'))
         table.to_csv(Filepath.APPLICANTS_DATA, index=False)
+        return
 
-    def _refresh_actuarial_data(self) -> None:
+    def _download_actuarial_data(self) -> None:
         url = 'https://www.ssa.gov/oact/HistEst/CohLifeTables/{0}/CohLifeTables_{1}_Alt2_TR{0}.txt'
         columns = {'Year': 'year', 'x': 'age', 'l(x)': 'survivors'}
         for s in ('F', 'M'):
-            response = self._session.get(url.format(Year.MAX_YEAR + 1, s))
+            response = self._session.get(url.format(self._max_year + 1, s))
             if not response.ok:
                 return
             sleep(3)
@@ -72,11 +63,13 @@ class SsaDataRefresher:
             for col in df.columns:
                 df[col] = df[col].map(int)
             df.to_csv(Filepath.ACTUARIAL.format(sex=s.lower()), index=False)
+        return
 
 
 def main() -> None:
-    refresher = SsaDataRefresher()
-    refresher.refresh()
+    downloader = SsaDataDownloader(2025)
+    downloader.download()
+    return
 
 
 if __name__ == '__main__':
