e82271d9887014f9f8f41f42ba3268f2dd8d82bd
Devon Ankar <devon.ankar@gmail.com>
2025-01-13 23:14:36 -0500

    names_by_peak: eliminate local var

--- names_by_peak.py
+++ names_by_peak.py
@@ -7,8 +7,8 @@ _GENDER_CATEGORY_AFTER: int = 1960
 
 
 def build_gender_ratio_after_year(raw: pd.DataFrame) -> pd.DataFrame:
-    years = list(range(_GENDER_CATEGORY_AFTER, Year.MAX_YEAR + 1, 10))
-    ratios = pd.concat(raw.loc[raw.year >= year, ['name', 'sex', 'number']].assign(after=year) for year in years)
+    ratios = pd.concat(raw.loc[raw.year >= year, ['name', 'sex', 'number']].assign(after=year) for year in range(
+        _GENDER_CATEGORY_AFTER, Year.MAX_YEAR + 1, 10))
 
     totals_by_name = ratios.groupby(['name', 'after'], as_index=False).number.sum()
 
